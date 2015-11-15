5f7bf932fa73cd1f15b023e38b495e34839898fe
louis <amoros.louis@gmail.com>
2015-11-15 12:13:37 +0100

    Create UserModal in common package in ejb session client. Updte sendMessage string and sendMessage user in messageSender class.

--- middle-auth-watcher-ejb/ejbModule/ejb/MessageSender.java
+++ middle-auth-watcher-ejb/ejbModule/ejb/MessageSender.java
@@ -1,11 +1,19 @@
 package ejb;
 
+import java.io.Serializable;
+
 import javax.annotation.Resource;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSRuntimeException;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
 import javax.jms.Queue;
+
+import common.UserModel;
 
 /**
  * Session Bean implementation class MessageSender
@@ -25,6 +33,25 @@ public class MessageSender implements MessageSenderLocal {
 	 */
 	public MessageSender() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void sendMessage(String message) {
+		try {
+			context.createProducer().send(queue, message);			
+		} catch (JMSRuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendMessage(UserModel user) {
+		try {
+			ObjectMessage message = context.createObjectMessage();
+			message.setObject((Serializable) user);
+			context.createProducer().send(queue, (Message) user);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
